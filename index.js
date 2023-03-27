0b5f99a892893b9ebf174d105e8edd0db0b3f9df
Valentine <kerubovalentine1@gmail.com>
2023-03-27 06:40:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+function  fetchBooks(){ 
+  // To pass the tests, don't forget to return your fetch! 
+   
+  // Sending a fetch request 
+  const fetchGame = fetch('https://anapioficeandfire.com/api/books'); 
+  return fetchGame.then(res => {return res.json()}).then(data => {renderBooks(data)}) 
+} 
+   
+  function renderBooks(books) { 
+   const main = document.querySelector('main'); 
+   books.forEach(book => { 
+     const h2 = document.createElement('h2'); 
+     h2.innerHTML = book.name; 
+     main.appendChild(h2); 
+    }); 
+  } 
+   
+  document.addEventListener('DOMContentLoaded', function() { 
+   fetchBooks(); 
+  });
